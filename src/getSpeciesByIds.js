76c6ea64f3467710b79250ee5aefa9f082ac0c3f
Robert Santos <robertdosantos09@gmail.com>
2022-06-29 18:59:49 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const res = [];
+  ids.forEach((v) => {
+    data.species.filter((e) => (e.id === v ? res.push(e) : ''));
+  });
+  return res;
 }
 
 module.exports = getSpeciesByIds;
