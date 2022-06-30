5453946f6dfa4af42db9352a23ce182473f698d6
Robert Santos <robertdosantos09@gmail.com>
2022-06-30 01:57:56 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const cond = (animal) => {
+  let res = 0;
+  const val = Object.values(animal);
+  data.species.forEach((e) => {
+    if (val.length === 1 && e.name === val[0]) {
+      res = e.residents.length;
+    }
+    e.residents.forEach((v) => {
+      if (e.name === val[0] && v.sex === val[1]) {
+        res += 1;
+      }
+    });
+  });
+  return res;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (!animal) {
+    data.species.forEach((e) => {
+      obj[e.name] = e.residents.length;
+    });
+    return obj;
+  }
+  return cond(animal);
 }
 
 module.exports = countAnimals;
