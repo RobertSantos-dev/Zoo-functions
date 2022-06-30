73cf809f94231fb52eb059489b3803a4858df0e5
Robert Santos <robertdosantos09@gmail.com>
2022-06-30 19:31:24 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const res = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((e) => {
+    if (e.age < 18) {
+      res.child += 1;
+    } else if (e.age >= 18 && e.age < 50) {
+      res.adult += 1;
+    } else { res.senior += 1; }
+  });
+  return res;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) { return 0; }
+  const obj = countEntrants(entrants);
+  const mul = Object.values(prices);
+  const res = Object.values(obj);
+  res[0] *= mul[0];
+  res[1] *= mul[2];
+  res[2] *= mul[1];
+
+  return res.reduce((acc, at) => acc + at);
 }
 
 module.exports = { calculateEntry, countEntrants };
