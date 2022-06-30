70dd5de04c66d6047c1e089ed4d764ea52e66b9d
Robert Santos <robertdosantos09@gmail.com>
2022-06-29 22:20:37 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+function isManager(...id) {
+  let res = false;
+  id.forEach((e) => {
+    if (e === stephanieId || e === olaId || e === burlId) {
+      res = true;
+    }
+  });
+  return res;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const r = [];
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  data.employees.forEach((e) => {
+    e.managers.forEach((v) => {
+      if (v === managerId) { r.push(`${e.firstName} ${e.lastName}`); }
+    });
+  });
+  return r;
 }
 
 module.exports = { isManager, getRelatedEmployees };
