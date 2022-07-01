6d529dd982e95afc8d280dc04764ce782e0d5e46
Robert Santos <robertdosantos09@gmail.com>
2022-07-01 17:01:39 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
+const procv = (id) => employees.filter((e) => e.id.includes(id))[0].responsibleFor;
+const procv2 = (arr) => species.find((e) => arr.find((e2) => e2 === e.id));
+const resEnd = (arr) => {
+  const res = [];
+  const obj = arr.residents.reduce((acc, at) => (at.age > acc.age ? at : acc));
+  Object.values(obj).forEach((e) => res.push(e));
+  return res;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const res = procv2(procv(id));
+  return resEnd(res);
 }
 
 module.exports = getOldestFromFirstSpecies;
